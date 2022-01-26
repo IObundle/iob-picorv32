01689472f9f972f7837f1aeefc6257e614db18e3
Jose T. de Sousa <jose.t.de.sousa@gmail.com>
2022-01-26 01:37:13 +0000

    fix bugs

--- hardware/hardware.mk
+++ hardware/hardware.mk
@@ -1,10 +1,13 @@
 ifeq ($(filter PICORV32, $(HW_MODULES)),)
 
-include $(CPU_DIR)/config.mk
+include $(PICORV32_DIR)/config.mk
 
 HW_MODULES+=PICORV32
 
-VSRC+=$(CPU_SRC_DIR)/picorv32.v $(CPU_SRC_DIR)/iob_picorv32.v
+#PATHS
+PICORV32_SRC_DIR=$(PICORV32_DIR)/hardware/src
+
+VSRC+=$(PICORV32_SRC_DIR)/picorv32.v $(PICORV32_SRC_DIR)/iob_picorv32.v
 
 #use hard multiplier and divider instructions
 DEFINE+=$(defmacro)USE_MUL_DIV=$(USE_MUL_DIV)
