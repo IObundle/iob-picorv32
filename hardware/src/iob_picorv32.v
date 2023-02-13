d7f50db4ecd9910e06b3b1da22641f9717ca9a75
Jose T. de Sousa <jose.t.de.sousa@gmail.com>
2023-02-13 02:22:26 +0000

    begin code revamp

--- hardware/src/iob_picorv32.v
+++ hardware/src/iob_picorv32.v
@@ -25,117 +25,106 @@
 //the look ahead interface is not working because mem_instr is unknown at request
 //`define LA_IF
 
-module iob_picorv32 #(
-    `include "iob_picorv32_params.vh"
-    ) (
-    input               clk_i,
-    input               rst_i,
-    input               cke_i,
-    input               boot,
-    output              trap,
+module iob_picorv32 
+  (
+   input         clk_i,
+   input         rst_i,
+   input         cke_i,
+   output        trap,
 
-    // instruction bus
-    output [`REQ_W-1:0] ibus_req,
-    input [`RESP_W-1:0] ibus_resp,
+   // instruction bus
+   output        ibus_avalid,
+   output        ibus_aready,
+   output [31:0] ibus_address,
+   input [31:0]  ibus_rdata,
+   input         ibus_rvalid,
 
-    // data bus
-    output [`REQ_W-1:0] dbus_req,
-    input [`RESP_W-1:0] dbus_resp
+   // data bus
+   output        dbus_avalid,
+   output [31:0] dbus_address,
+   output [31:0] dbus_wdata,
+   output [3:0]  dbus_wstrb,
+   input [31:0]  dbus_rdata,
+   input         dbus_rvalid,
+   input         dbus_ready
     );
 
-   //create picorv32 native interface concat buses
-   wire [1*`REQ_W-1:0]  cpu_req, cpu_i_req, cpu_d_req;
-   wire [1*`RESP_W-1:0] cpu_resp;
 
-   //modify addresses if DDR used according to boot status
-   generate
-      if (USE_EXTMEM) begin
-         assign ibus_req = {cpu_i_req[V_BIT], ~boot, cpu_i_req[`REQ_W-3:0]};
-         assign dbus_req = {cpu_d_req[V_BIT], (cpu_d_req[E_BIT]^~boot)&~cpu_d_req[P_BIT], cpu_d_req[`REQ_W-3:0]};
-      end else begin
-         assign ibus_req = cpu_i_req;
-         assign dbus_req = cpu_d_req;
-      end
-   endgenerate
-
+   // cpu bus
+   wire cpu_avalid;
+   wire [31:0] cpu_address;
+   wire [31:0] cpu_wdata;
+   wire [3:0] cpu_wstrb;
+   wire [31:0] cpu_rdata;
+   wire cpu_ready;
+   
+   // write acknowledge
+   wire dbus_wack;
+   wire dbus_wack_nxt = cpu_avalid & cpu_ready & (| cpu_wstrb) ;
+   iob_reg #(1,0) wack_reg 
+     (
+      .clk_i(clk_i),
+      .rst_i(rst_i),
+      .d_i(dbus_wack_nxt),
+      .q_o(dbus_wack)
+      );
+      
    //split cpu bus into instruction and data buses
    wire   cpu_instr;
-   assign cpu_i_req = cpu_instr?  cpu_req : {`REQ_W{1'b0}};
-   assign cpu_d_req = !cpu_instr? cpu_req : {`REQ_W{1'b0}};
-   assign cpu_resp = cpu_instr? ibus_resp: dbus_resp;
-   
-   reg iob_wack;
-   wire cpu_avalid;
-   wire [`WSTRB_W-1:0] cpu_wstrb;
-   assign cpu_req[`wstrb(0)] = cpu_wstrb;
-   wire iob_rvalid = cpu_resp[`rvalid(0)];
-   wire iob_ready  = cpu_resp[`ready(0)];
-   wire cpu_ready  = (iob_rvalid | iob_wack) & cpu_avalid;
-
-   wire iob_wack_nxt = cpu_avalid & (| cpu_wstrb) & iob_ready;
-   iob_reg #(1,0) wack_reg (clk_i, rst_i, cke_i, iob_wack_nxt, iob_wack);
-
-`ifdef LA_IF
-   wire mem_la_read, mem_la_write;
-   always @(posedge clk_i) cpu_avalid <= mem_la_read | mem_la_write;
-`else
-   assign cpu_req[`avalid(0)] = cpu_avalid & ~cpu_ready;
-`endif
-   
+   assign ibus_avalid = cpu_avalid & cpu_instr;
+   assign dbus_avalid = cpu_avalid & -cpu_instr;
+   assign ibus_address = cpu_address;
+   assign dbus_address = cpu_address;
+   assign dbus_wdata = cpu_wdata;
+   assign dbus_wstrb = cpu_wstrb;
+   assign cpu_rdata = cpu_instr? ibus_rdata : dbus_rdata;
+   assign cpu_ready = (ibus_rvalid | dbus_rvalid | iob_wack) & cpu_avalid;
 
    //intantiate picorv32
-   picorv32 #(
-              .COMPRESSED_ISA(USE_COMPRESSED),
-              .ENABLE_FAST_MUL(USE_MUL_DIV),
-              .ENABLE_DIV(USE_MUL_DIV),
-              .BARREL_SHIFTER(1)
-              )
-   picorv32_core (
-                  .clk           (clk_i),
-                  .resetn        (~rst_i),
-                  .trap          (trap),
-                  .mem_instr     (cpu_instr),
-`ifndef LA_IF
-                  //memory interface
-                  .mem_valid     (cpu_avalid),
-                  .mem_addr      (cpu_req[`address(0, ADDR_W)]),
-                  .mem_wdata     (cpu_req[`wdata(0)]),
-                  .mem_wstrb     (cpu_wstrb),
-                  //lookahead interface
-                  .mem_la_read   (),
-                  .mem_la_write  (),
-                  .mem_la_addr   (),
-                  .mem_la_wdata  (),
-                  .mem_la_wstrb  (),
-`else
-                  //memory interface
-                  .mem_valid     (),
-                  .mem_addr      (),
-                  .mem_wdata     (),
-                  .mem_wstrb     (),
-                  //lookahead interface
-                  .mem_la_read   (mem_la_read),
-                  .mem_la_write  (mem_la_write),
-                  .mem_la_addr   (cpu_req[`address(0, ADDR_W)]),
-                  .mem_la_wdata  (cpu_req[`wdata(0)]),
-                  .mem_la_wstrb  (cpu_wstrb),
-`endif
-                  .mem_rdata     (cpu_resp[`rdata(0)]),
-                  .mem_ready     (cpu_ready),
-                  //co-processor interface (PCPI)
-                  .pcpi_valid    (),
-                  .pcpi_insn     (),
-                  .pcpi_rs1      (),
-                  .pcpi_rs2      (),
-                  .pcpi_wr       (1'b0),
-                  .pcpi_rd       (32'd0),
-                  .pcpi_wait     (1'b0),
-                  .pcpi_ready    (1'b0),
-                  // IRQ
-                  .irq           (32'd0),
-                  .eoi           (),
-                  .trace_valid   (),
-                  .trace_data    ()                  
-                  );
+   picorv32 
+     #(
+       .COMPRESSED_ISA(1),
+       .ENABLE_FAST_MUL(1),
+       .ENABLE_DIV(1),
+       .BARREL_SHIFTER(1),
+       .PROGADDR_RESET(32'h 1000_0000)
+       )
+   picorv32_core 
+     (
+      .clk           (clk_i),
+      .resetn        (~rst_i),
+      .trap          (trap),
+      .mem_instr     (cpu_instr),
+
+      //memory interface
+      .mem_valid     (cpu_avalid),
+      .mem_addr      (cpu_address),
+      .mem_wdata     (cpu_wdata),
+      .mem_wstrb     (cpu_wstrb),
+      .mem_rdata     (cpu_rdata),
+      .mem_ready     (cpu_ready),
+
+      //lookahead interface
+      .mem_la_read   (),
+      .mem_la_write  (),
+      .mem_la_addr   (),
+      .mem_la_wdata  (),
+      .mem_la_wstrb  (),
+
+      //co-processor interface (PCPI)
+      .pcpi_valid    (),
+      .pcpi_insn     (),
+      .pcpi_rs1      (),
+      .pcpi_rs2      (),
+      .pcpi_wr       (1'b0),
+      .pcpi_rd       (32'd0),
+      .pcpi_wait     (1'b0),
+      .pcpi_ready    (1'b0),
+      // IRQ
+      .irq           (32'd0),
+      .eoi           (),
+      .trace_valid   (),
+      .trace_data    ()                  
+      );
    
 endmodule
