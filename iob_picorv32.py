f7c0f18cb4faca6efd68c91b860137bd685cc0d9
Artur Nóbrega <arturnobrega1999@gmail.com>
2024-02-28 03:55:37 +0000

    feat(setup_dir): Remove setup_dir attribute

--- iob_picorv32.py
+++ iob_picorv32.py
@@ -11,7 +11,6 @@ class iob_picorv32(iob_module):
     def __init__(self):
         super().__init__()
         self.version = 'V0.10'
-        self.setup_dir = os.path.dirname(__file__)
         self.submodule_list = [
             iob_reg(),
             iob_edge_detect(),
