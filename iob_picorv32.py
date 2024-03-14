c7785bda1487299315575f9f81ed124b48b0aa0d
PMiranda <pmiranda.iobundle@gmail.com>
2024-03-14 18:06:08 +0000

    feat(REQ): replace REQ/RESP buses - replace REQ/RESP buses with iob native interfaces

--- iob_picorv32.py
+++ iob_picorv32.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 
-import os
-
 from iob_module import iob_module
 from iob_reg import iob_reg
 from iob_edge_detect import iob_edge_detect
@@ -10,20 +8,54 @@ from iob_edge_detect import iob_edge_detect
 class iob_picorv32(iob_module):
     def __init__(self):
         super().__init__()
-        self.version = 'V0.10'
+        self.version = "V0.10"
         self.submodule_list = [
             iob_reg(),
             iob_edge_detect(),
         ]
         self.confs = [
             # Macros
-
             # Parameters
-            {'name':'ADDR_W', 'type':'P', 'val':'32', 'min':'1', 'max':'?', 'descr':'description here'},
-            {'name':'DATA_W', 'type':'P', 'val':'32', 'min':'1', 'max':'?', 'descr':'description here'},
-            {'name':'USE_COMPRESSED', 'type':'P', 'val':'1', 'min':'0', 'max':'1', 'descr':'description here'},
-            {'name':'USE_MUL_DIV', 'type':'P', 'val':'1', 'min':'0', 'max':'1', 'descr':'description here'},
-            {'name':'USE_EXTMEM', 'type':'P', 'val':'0', 'min':'0', 'max':'1', 'descr':'Select if configured for usage with external memory.'},
+            {
+                "name": "ADDR_W",
+                "type": "P",
+                "val": "32",
+                "min": "1",
+                "max": "?",
+                "descr": "description here",
+            },
+            {
+                "name": "DATA_W",
+                "type": "P",
+                "val": "32",
+                "min": "1",
+                "max": "?",
+                "descr": "description here",
+            },
+            {
+                "name": "USE_COMPRESSED",
+                "type": "P",
+                "val": "1",
+                "min": "0",
+                "max": "1",
+                "descr": "description here",
+            },
+            {
+                "name": "USE_MUL_DIV",
+                "type": "P",
+                "val": "1",
+                "min": "0",
+                "max": "1",
+                "descr": "description here",
+            },
+            {
+                "name": "USE_EXTMEM",
+                "type": "P",
+                "val": "0",
+                "min": "0",
+                "max": "1",
+                "descr": "Select if configured for usage with external memory.",
+            },
         ]
         self.ios = [
             {
@@ -35,63 +67,53 @@ class iob_picorv32(iob_module):
                 "ports": [],
             },
             {
-                'name': 'general',
+                "name": "general",
                 "type": "master",
                 "port_prefix": "",
                 "wire_prefix": "",
-                'descr':'General interface signals',
-                'ports': [
+                "descr": "General interface signals",
+                "ports": [
                     {
-                        'name':"boot",
-                        'direction':"input",
-                        'width':'1',
-                        'descr':"CPU boot input"
+                        "name": "boot",
+                        "direction": "input",
+                        "width": "1",
+                        "descr": "CPU boot input",
                     },
                     {
-                        'name':"trap",
-                        'direction':"output",
-                        'width':'1',
-                        'descr':"CPU trap output"
+                        "name": "trap",
+                        "direction": "output",
+                        "width": "1",
+                        "descr": "CPU trap output",
                     },
-                ]},
+                ],
+            },
             {
-                'name': 'instruction_bus',
+                "name": "iob",
                 "type": "master",
-                "port_prefix": "",
-                "wire_prefix": "",
-                'descr':'Instruction bus',
-                'ports': [
-                    {
-                        'name':"ibus_req",
-                        'direction':"output",
-                        'width':'`REQ_W',
-                        'descr':"Instruction bus request"},
-                    {
-                        'name':"ibus_resp",
-                        'direction':"input",
-                        'width':'`RESP_W',
-                        'descr':"Instruction bus response"
-                    },
-                ]},
+                "file_prefix": "iob_picorv32_ibus_",
+                "port_prefix": "ibus_",
+                "wire_prefix": "ibus_",
+                "param_prefix": "",
+                "descr": "iob-picorv32 instruction bus",
+                "ports": [],
+                "widths": {
+                    "DATA_W": "DATA_W",
+                    "ADDR_W": "ADDR_W",
+                },
+            },
             {
-                'name': 'data_bus',
+                "name": "iob",
                 "type": "master",
-                "port_prefix": "",
-                "wire_prefix": "",
-                'descr':'Data bus',
-                'ports': [
-                    {
-                        'name':"dbus_req",
-                        'direction':"output",
-                        'width':'`REQ_W',
-                        'descr':"Data bus request"
-                    },
-                    {
-                        'name':"dbus_resp",
-                        'direction':"input",
-                        'width':'`RESP_W',
-                        'descr':"Data bus response"
-                    },
-                ]}
+                "file_prefix": "iob_picorv32_dbus_",
+                "port_prefix": "dbus_",
+                "wire_prefix": "dbus_",
+                "param_prefix": "",
+                "descr": "iob-picorv32 data bus",
+                "ports": [],
+                "widths": {
+                    "DATA_W": "DATA_W",
+                    "ADDR_W": "ADDR_W",
+                },
+            },
         ]
         self.block_groups = []
